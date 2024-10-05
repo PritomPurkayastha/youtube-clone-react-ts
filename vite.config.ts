cf6e0ad44cb74d3cc8ac186bd5ec35b33a9a1ebb
ppurkayastha <ppurkayastha@workllama.com>
2024-10-05 14:46:47 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/youtube-clone-react-ts",
+  base: "/youtube-clone-react-ts/",
   plugins: [react()],
   envPrefix: 'VITE_',
 })
