88ce25943bab840008c8e98b0866f39146adc6c6
Pritom <prkpritom83@gmail.com>
2025-12-01 21:48:16 +0530

    play video on reload

--- src/pages/watchPage/index.tsx
+++ src/pages/watchPage/index.tsx
@@ -3,23 +3,47 @@ import Video from './Video'
 import Comments from './Comments'
 import RecommendedVideos from './RecommendedVideos'
 import Description from './Description'
-import { YouTubeVideo } from "../../store/slices/videoSlice";
+import { fetchSingleVideoData, YouTubeVideo } from "../../store/slices/videoSlice";
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from '../../store/store'
-import { fetchTopLevelComments } from '../../store/slices/commentSlice'
+import { fetchTopLevelComments, resetComments } from '../../store/slices/commentSlice'
 import useGetCurrentVideoData from '../../utils/useGetCurrentVideoData'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const WatchPage = () => {
   type videoType = YouTubeVideo | null;
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const currentVideo: videoType = useSelector((state: RootState) => state.video.currentVideoData);
+  const [searchParams] = useSearchParams();
+  const videoId: string | null = searchParams.get('v');
   useGetCurrentVideoData();
 
   useEffect(() => {
-    if (currentVideo) {
-      dispatch(fetchTopLevelComments({ videoId: currentVideo.id }));
+    fetchVideoData();
+  },[])
+
+  const fetchVideoData = async () => {
+    if (!videoId) return;
+    const resultAction = await dispatch(fetchSingleVideoData(videoId));
+    try {
+      if (fetchSingleVideoData.fulfilled.match(resultAction)) {
+        sessionStorage.setItem(
+          "video",
+          JSON.stringify(resultAction.payload.items[0])
+        );
+        dispatch(resetComments());
+      } else if (fetchSingleVideoData.rejected.match(resultAction)) {
+        console.error(
+          "Failed to fetch video data: ",
+          resultAction.error.message
+        );
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching video data:", error);
     }
-  },[currentVideo])
+  }
+
   return (
     <div className='grid grid-cols-1 lg:grid-cols-12 px-4 xl:pl-28 h-full'>
       <div className='col-span-12 lg:col-span-8 flex flex-col items-start'>
